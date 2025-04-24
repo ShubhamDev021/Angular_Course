e50f8df185417ce059b2cab0893ee8540f425c53
ShubhamDev021 <shubhat021@gmail.com>
2025-04-24 19:59:49 +0530

    Components in angular - Creating a custom component

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-profile',
+    template: `<h1>Profile component</h1>`,
+    styles: 'h1 {color: orange;}'
+})
+
+export class ProfileComponent {
+
+}
